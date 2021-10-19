5b9800ffc7a608cbf0fc0291003a0720e3eeb8d8
amakaogujiofor <80483692+amakaogujiofor@users.noreply.github.com>
2021-10-19 15:12:53 +0100

    Created new variables for the game

---
+++ script.js
@@ -0,0 +1,5 @@
+// Declare Game Stats
+let player1Score = document.getElementById("player1Scoreboard");
+let player2Score = document.getElementById("player2Scoreboard");
+let player1Turn = true;
+let scores = document.getElementById();
